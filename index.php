529156a5ff82cac91661e30afa4290a87040aafb
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-05-31 23:56:23 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<DOCTYPE HTML>
+    <head>
+        <style>
+
+            body{
+                background-image: url("https://cdn.mos.cms.futurecdn.net/LDpg7jwzPsuwgT82a3jKW7.jpg");
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: 100%;
+            }
+
+            h1 {
+                border: 0px solid black;
+            -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease;
+
+            }
+
+            h1:hover {
+                transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+            }
+
+            .topnav {
+                background-color: white;
+                overflow: hidden;
+            }
+
+            .topnav a {
+
+            }
+
+            img {
+     width: 400px;
+     height: 400px;
+	 border: 0px solid black;
+	 padding: 5px;
+   }
+   img:hover {
+      box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.5);
+   }
+
+            .thumbnail { 
+        display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+    
+} 
+
+.thumbnail:hover { 
+        transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.games {
+    display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+}
+
+
+
+        </style>
+    <title>Arakko is for games</title>
+
+
+    </head>
+
+<body>
+
+<a href="http://localhost/arakko" align="center">Index</a>
+<a href="http://localhost/arakko/books.php" align="center">Books</a>
+<a href="http://localhost/arakko/skills.php" align="center">skills</a>
+
+
+
+
+<h1 align="center" class="current game of the year"><img src="https://playsense.nl/wp-content/uploads/2024/05/stellar-blade-eve-kijkt-zwoel.jpg"></h1>
+
+<div class="Completed game" align="center">
+
+<a href="https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TalesOfKenzeraZau.jpg" class="thumbnail">
+    <img src="https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TalesOfKenzeraZau.jpg" alt="Tales of Kenzera:Zau">
+</a>
+
+<a href="https://i0.wp.com/insider-gaming.com/wp-content/uploads/2024/02/stellar-blade-eve.jpg?fit=2560%2C1440&ssl=1" class="thumbnail">
+    <img src="https://i0.wp.com/insider-gaming.com/wp-content/uploads/2024/02/stellar-blade-eve.jpg?fit=2560%2C1440&ssl=1" alt="Stellar Blade">
+</a>
+
+</div>
+
+<div class="Current game" align="center">
+
+<a href="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" class="thumbnail">
+    <img src="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" alt="Final Fantasys 7 rebirth">
+</a>
+
+</div>
+
+
+<div class="scroll-container2" align="center">
+
+<a href="https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg" class="thumbnail">
+    <img src="https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg" alt="Baldur's Gate 3">
+</a>
+
+<a href="https://image.api.playstation.com/vulcan/ap/rnd/202402/1519/a7151c293ebd9344b6521a2dad4896411f78a9226fd8a74a.jpg" class="thumbnail">
+    <img src="https://image.api.playstation.com/vulcan/ap/rnd/202402/1519/a7151c293ebd9344b6521a2dad4896411f78a9226fd8a74a.jpg" alt="Shin Megami Tensei V: Vengeance">
+</a>
+
+<a href="https://image.api.playstation.com/vulcan/ap/rnd/202402/0713/5eec986de2675a1514f58d8ba40f3ee13a6faeefc4cf6d30.png" class="thumbnail">
+    <img src="https://image.api.playstation.com/vulcan/ap/rnd/202402/0713/5eec986de2675a1514f58d8ba40f3ee13a6faeefc4cf6d30.png" alt="Elden Ring: Shadow of the Erdtree">
+</a>
+
+
+</div>
+
+<div class="scroll-container3" align="center">
+
+<a href="https://sm.ign.com/ign_nl/news/p/persona-3-/persona-3-4-and-5-were-blue-yellow-and-red-and-persona-6-wil_ryps.jpg" class="thumbnail">
+    <img src="https://sm.ign.com/ign_nl/news/p/persona-3-/persona-3-4-and-5-were-blue-yellow-and-red-and-persona-6-wil_ryps.jpg" alt="Persona 6">
+</a>
+
+
+<a href="https://i.pinimg.com/originals/e1/bf/64/e1bf640df96495a7adeb6bf2f57c1841.jpg" class="thumbnail">
+    <img src="https://i.pinimg.com/originals/e1/bf/64/e1bf640df96495a7adeb6bf2f57c1841.jpg" alt="Horizon III">
+</a>
+
+<a href="https://www.datocms-assets.com/56706/1707465398-grand-theft-auto-6.png?auto=format&fit=max&w=1200" class="thumbnail">
+    <img src="https://www.datocms-assets.com/56706/1707465398-grand-theft-auto-6.png?auto=format&fit=max&w=1200" alt="Grand Theft Auto 6">
+</a>
+
+<a href="https://yt3.googleusercontent.com/skAODIZ07KvllI7tXL6ERzwZn1ECRCdIPehY4dV5AvaD9xxu6L2Q-rm6his2ImwQgSGISmJtgg=s900-c-k-c0x00ffffff-no-rj" class="thumbnail">
+    <img src="https://yt3.googleusercontent.com/skAODIZ07KvllI7tXL6ERzwZn1ECRCdIPehY4dV5AvaD9xxu6L2Q-rm6his2ImwQgSGISmJtgg=s900-c-k-c0x00ffffff-no-rj" alt="Cyberpunk">
+</a>
+
+</div>
+
+
+</body>
+</HTML>
---
+++ skills.php
@@ -0,0 +1,126 @@
+<DOCTYPE HTML>
+    <head>
+        <style>
+
+            body{
+                background-image: url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2f6ed149-c2f1-45b1-876c-0c26ccf7c2e2/dfwf04p-132be7c2-bbe3-4199-9c7c-d12a3d05dd30.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJmNmVkMTQ5LWMyZjEtNDViMS04NzZjLTBjMjZjY2Y3YzJlMlwvZGZ3ZjA0cC0xMzJiZTdjMi1iYmUzLTQxOTktOWM3Yy1kMTJhM2QwNWRkMzAuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.KGIhmvgSw6oHGfiFtOo4hC6kPlJp7vCMkUHlpjvShSE");
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: 100%;
+            }
+
+            h1 {
+                border: 0px solid black;
+            -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease;
+
+            }
+
+            h1:hover {
+                transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+            }
+
+            .topnav {
+                background-color: white;
+                overflow: hidden;
+            }
+
+            .topnav a {
+
+            }
+
+            img {
+     width: 400px;
+     height: 400px;
+	 border: 0px solid black;
+	 padding: 5px;
+   }
+   img:hover {
+      box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.5);
+   }
+
+            .thumbnail { 
+        display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+    
+} 
+
+.thumbnail:hover { 
+        transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.games {
+    display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+}
+
+
+
+        </style>
+    <title>Arakko is for games</title>
+
+
+    </head>
+
+<body>
+
+<a href="http://localhost/arakko/" align="center">Home</a>
+<a href="http://localhost/arakko/books.php" align="center">Books</a>
+<a href="http://localhost/arakko/skills.php" align="center">Skills</a>
+
+
+
+<h1 align="center" class="What I wish to become"><img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/12/cyberpunk-red-netrunning-1.jpg"></h1>
+
+<div class="Priority 1 skills" align="center">
+
+
+<a href="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2016/06/how-to-change-your-php-version.webp" class="thumbnail">
+    <img src="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2016/06/how-to-change-your-php-version.webp" alt="PHP">
+</a>
+
+<a href="https://miro.medium.com/v2/resize:fit:792/1*lJ32Bl-lHWmNMUSiSq17gQ.png" class="thumbnail">
+        <img src="https://miro.medium.com/v2/resize:fit:792/1*lJ32Bl-lHWmNMUSiSq17gQ.png" alt="HTML CSS">
+
+</div>
+
+<div class="Priority 2 skills" align="center">
+    <a href="https://ioflood.com/blog/wp-content/uploads/2023/09/Collage-of-Python-programming-aspects-syntax-libraries-Python-symbols-logo.jpg" class="thumbnail">
+        <img src="https://ioflood.com/blog/wp-content/uploads/2023/09/Collage-of-Python-programming-aspects-syntax-libraries-Python-symbols-logo.jpg" alt="Python">
+
+        <a href="https://media.licdn.com/dms/image/D4E12AQFfe1nZbaWdMw/article-cover_image-shrink_720_1280/0/1698604163003?e=2147483647&v=beta&t=rtD52hfy37nFVmc4_MXfnflV6C-ke773W70SYJLoWRc" class="thumbnail">
+        <img src="https://media.licdn.com/dms/image/D4E12AQFfe1nZbaWdMw/article-cover_image-shrink_720_1280/0/1698604163003?e=2147483647&v=beta&t=rtD52hfy37nFVmc4_MXfnflV6C-ke773W70SYJLoWRc" alt="JavaScript">
+
+        <a href="https://undsgn.com/wp-content/uploads/2018/03/XAMPP-3.jpg" class="thumbnail">
+        <img src="https://undsgn.com/wp-content/uploads/2018/03/XAMPP-3.jpg" alt="XAMPP">
+
+</div>
+
+<div class="Priority 3 skils" align="center">
+
+<a href="https://thetranslationcompany.com/wp-content/uploads/2022/09/The-Colombian-Spanish-Dialect.jpg" class="thumbnail">
+        <img src="https://thetranslationcompany.com/wp-content/uploads/2022/09/The-Colombian-Spanish-Dialect.jpg" alt="Colombian Spanish">
+
+        <a href="https://online.york.ac.uk/wp-content/uploads/2023/10/Cryptography.jpg" class="thumbnail">
+        <img src="https://online.york.ac.uk/wp-content/uploads/2023/10/Cryptography.jpg" alt="Cryptography">
+
+        <a href="https://www.webfx.com/wp-content/uploads/2022/08/github-logo.png" class="thumbnail">
+            <img src="https://www.webfx.com/wp-content/uploads/2022/08/github-logo.png" alt="Github">
+
+        <a href="https://sp-ao.shortpixel.ai/client/to_webp,q_lossless,ret_img,w_680,h_680/https://www.gallerysystems.com/wp-content/uploads/SQL-server.jpg" class="thumbnail">
+            <img src="https://sp-ao.shortpixel.ai/client/to_webp,q_lossless,ret_img,w_680,h_680/https://www.gallerysystems.com/wp-content/uploads/SQL-server.jpg" alt="sql">
+
+</div>
+
+
+
+</body>
+</HTML>
