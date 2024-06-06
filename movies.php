b58b2749a7b5657d58a475c8ce30aea592a3eb22
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-06-07 00:00:23 +0200

    Add files via upload

---
+++ movies.php
@@ -0,0 +1,166 @@
+<DOCTYPE HTML>
+    <head>
+        <style>
+
+            body{
+                background-image: url("https://basecamplive.com/wp-content/uploads/2023/02/books.jpeg");
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: 100%;
+            }
+
+            h1 {
+                border: 0px solid black;
+            -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease;
+
+            }
+
+            h1:hover {
+                transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+            }
+
+            .topnav {
+                background-color: white;
+                overflow: hidden;
+            }
+
+            .topnav a {
+
+            }
+
+            img {
+     width: 400px;
+     height: 400px;
+	 border: 0px solid black;
+	 padding: 5px;
+   }
+   img:hover {
+      box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.5);
+   }
+
+            .thumbnail { 
+        display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+    
+} 
+
+.thumbnail:hover { 
+        transform: scale(1.2);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.games {
+    display: inline-block;
+        -webkit-transition-property: all; 
+-webkit-transition-duration: 0.3s; 
+-webkit-transition-timing-function: ease; 
+}
+
+
+
+
+        </style>
+    <title>Watch and learn</title>
+
+
+    </head>
+
+<body>
+
+<div class='tv shows finished' align="center">
+
+<a href="https://variety.com/wp-content/uploads/2021/11/Screen-Shot-2021-11-12-at-9.25.46-AM.png" class="thubmnail">
+    <img src="https://variety.com/wp-content/uploads/2021/11/Screen-Shot-2021-11-12-at-9.25.46-AM.png" alt="X-Men 97">
+</a>
+
+</div>
+
+<div class="tv shows in june" align="center">
+
+<a href="https://m.media-amazon.com/images/M/MV5BNDMwZDA1ZDgtOWUyMi00NDBlLTkxODktYWNiODExZTQzMjU5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UX1000_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BNDMwZDA1ZDgtOWUyMi00NDBlLTkxODktYWNiODExZTQzMjU5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UX1000_.jpg" alt="Hit-Monkey">
+</a>
+
+<a href="https://preview.redd.it/u9p8gwgnqgb61.jpg?auto=webp&s=44d6e95987bb4a2274d3efd90e1e71c4bc905471" class="thumbnail">
+    <img src="https://preview.redd.it/u9p8gwgnqgb61.jpg?auto=webp&s=44d6e95987bb4a2274d3efd90e1e71c4bc905471" alt="Vivy Fluorite Eye Song">
+</a>
+
+</div>
+
+<div class="backburner" align="center">
+
+<a href="https://m.media-amazon.com/images/M/MV5BZGJiNmM1NDctNWUxYS00YzE4LWJjNTgtYTJhYzE0NjFmMTMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BZGJiNmM1NDctNWUxYS00YzE4LWJjNTgtYTJhYzE0NjFmMTMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg" alt="Bob's Burgers">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BNzI5ODk5MTMtMDE4OC00ZWM5LTg0MzYtNTJmZTY3OWY3NzEzXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BNzI5ODk5MTMtMDE4OC00ZWM5LTg0MzYtNTJmZTY3OWY3NzEzXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg" alt="King of the Hill">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BMDc3OGNhYjUtZGYwNi00MjllLWE0MjYtNDFiYmVhNWI0MGJmXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BMDc3OGNhYjUtZGYwNi00MjllLWE0MjYtNDFiYmVhNWI0MGJmXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_.jpg" alt="Star Trek Deep Space Nine">
+</a>
+
+
+</div>
+
+<div class="movies and tv finished" align="center">
+
+<a href="https://m.media-amazon.com/images/M/MV5BNmYzMWVjNmQtNjJjNy00M2Y4LTkzZjQtZWQ5NmYzMjRjMDIzXkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_FMjpg_UX1000_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BNmYzMWVjNmQtNjJjNy00M2Y4LTkzZjQtZWQ5NmYzMjRjMDIzXkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_FMjpg_UX1000_.jpg" alt="Furiosa">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BYTJmMDM4YWItMGM2YS00MzQ0LTliM2ItZDE1NzVlMjNjNDVlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BYTJmMDM4YWItMGM2YS00MzQ0LTliM2ItZDE1NzVlMjNjNDVlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" alt="Hit-Man">
+</a>
+
+<a href="https://media.pathe.nl/nocropthumb/620x955/gfx_content/posters/Love-Lies-Bleeding_ps_1_jpg_sd-low%20(1).jpg" class="thumbnail">
+    <img src="https://media.pathe.nl/nocropthumb/620x955/gfx_content/posters/Love-Lies-Bleeding_ps_1_jpg_sd-low%20(1).jpg" alt="Love Lies Bleeding">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BN2QyZGU4ZDctOWMzMy00NTc5LThlOGQtODhmNDI1NmY5YzAwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BN2QyZGU4ZDctOWMzMy00NTc5LThlOGQtODhmNDI1NmY5YzAwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" alt="Dune Part 2">
+</a>
+
+</div>
+
+
+<div class="movies" align="center">
+
+<a href="https://m.media-amazon.com/images/M/MV5BNDA3MmYzOWEtYWZhNS00ZmU3LWJkYmUtODBjMzQyNzMxYmZhXkEyXkFqcGc@._V1_.jpg" class="thubmnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BNDA3MmYzOWEtYWZhNS00ZmU3LWJkYmUtODBjMzQyNzMxYmZhXkEyXkFqcGc@._V1_.jpg" alt="Deadpool and Wolverine">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BNTI0MjIwMDAtYWY1ZC00MDUyLWI0MzMtZGNjODRlN2Y3ZmE2XkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_FMjpg_UX1000_.jpg" class="thubmnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BNTI0MjIwMDAtYWY1ZC00MDUyLWI0MzMtZGNjODRlN2Y3ZmE2XkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_FMjpg_UX1000_.jpg" alt="Joker 2">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BYTc1MDQ3NjAtOWEzMi00YzE1LWI2OWUtNjQ0OWJkMzI3MDhmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg" class="thubmnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BYTc1MDQ3NjAtOWEzMi00YzE1LWI2OWUtNjQ0OWJkMzI3MDhmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg" alt="Inside Out 2">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BMjA5ZjA3ZjMtMzg2ZC00ZDc4LTk3MTctYTE1ZTUzZDIzMjQyXkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_FMjpg_UX1000_.jpg" class="thubmnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BMjA5ZjA3ZjMtMzg2ZC00ZDc4LTk3MTctYTE1ZTUzZDIzMjQyXkEyXkFqcGdeQXVyMTM1NjM2ODg1._V1_FMjpg_UX1000_.jpg" alt="The Fall-guy">
+</a>
+
+<a href="https://upload.wikimedia.org/wikipedia/en/2/21/AngelsEgg1985.jpg" class="thumbnail">
+    <img src="https://upload.wikimedia.org/wikipedia/en/2/21/AngelsEgg1985.jpg" alt="Angel's Egg">
+</a>
+
+<a href="https://m.media-amazon.com/images/M/MV5BNmFlZjM3NGQtYmQzMC00MGIxLWE1ZDQtNmQzNjEzYWQ0MDI5XkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_FMjpg_UX1000_.jpg" class="thumbnail">
+    <img src="https://m.media-amazon.com/images/M/MV5BNmFlZjM3NGQtYmQzMC00MGIxLWE1ZDQtNmQzNjEzYWQ0MDI5XkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_FMjpg_UX1000_.jpg" alt="Metropolis">
+</a>
+
+</div>
+
+
+
+
+</body>
+</HTML>
