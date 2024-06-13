26542cee8f95dd95002a8b7fd1993b91eea084dc
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-06-13 19:12:01 +0200

    Update books.php

--- books.php
+++ books.php
@@ -84,35 +84,35 @@
 
 <div class="finished books" align="center">
     <a href="https://m.media-amazon.com/images/I/81uuRYwTwbL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81uuRYwTwbL._AC_UF894,1000_QL80_.jpg" alt="Unconquerable Sun">
+        <img src="https://m.media-amazon.com/images/I/81uuRYwTwbL._AC_UF894,1000_QL80_.jpg" alt="Unconquerable Sun"/>
     </a>
 
     <a href="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1611239361i/36205229.jpg" class="thumbnail">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1611239361i/36205229.jpg" alt="Furious Heaven">
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1611239361i/36205229.jpg" alt="Furious Heaven"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/71Ha3OShqSL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/71Ha3OShqSL._AC_UF1000,1000_QL80_.jpg" alt="The Body Keeps the score">
+        <img src="https://m.media-amazon.com/images/I/71Ha3OShqSL._AC_UF1000,1000_QL80_.jpg" alt="The Body Keeps the score"/>
     </a>
 
     <a href="https://i0.wp.com/i.annihil.us/u/prod/marvel/i/mg/9/60/62cf28372f4bb/clean.jpg?w=720&ssl=1" class="thumbnail">
-        <img src="https://i0.wp.com/i.annihil.us/u/prod/marvel/i/mg/9/60/62cf28372f4bb/clean.jpg?w=720&ssl=1" alt="Defenders: Beyond">
+        <img src="https://i0.wp.com/i.annihil.us/u/prod/marvel/i/mg/9/60/62cf28372f4bb/clean.jpg?w=720&ssl=1" alt="Defenders: Beyond"/>
     </a>
 
     <a href="https://media.s-bol.com/BRNNqVmLOxAJ/pQgLWlr/548x840.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/BRNNqVmLOxAJ/pQgLWlr/548x840.jpg" alt="My Grandmother's Hands">
+        <img src="https://media.s-bol.com/BRNNqVmLOxAJ/pQgLWlr/548x840.jpg" alt="My Grandmother's Hands"/>
     </a>
 
     <a href="https://www.tfaw.com/media/catalog/product/cache/1626744db03ed6f05af295b1a820c9c8/7/5/75960620491500511_1.jpg" class="thumbnail">
-        <img src="https://www.tfaw.com/media/catalog/product/cache/1626744db03ed6f05af295b1a820c9c8/7/5/75960620491500511_1.jpg" alt="I am Iron Man">
+        <img src="https://www.tfaw.com/media/catalog/product/cache/1626744db03ed6f05af295b1a820c9c8/7/5/75960620491500511_1.jpg" alt="I am Iron Man"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81vhLmlJoqL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81vhLmlJoqL._AC_UF1000,1000_QL80_.jpg" alt="Conversationally Speaking">
+        <img src="https://m.media-amazon.com/images/I/81vhLmlJoqL._AC_UF1000,1000_QL80_.jpg" alt="Conversationally Speaking"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81Jy2PU82kL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81Jy2PU82kL._AC_UF894,1000_QL80_.jpg" alt="Dopamine Nation">
+        <img src="https://m.media-amazon.com/images/I/81Jy2PU82kL._AC_UF894,1000_QL80_.jpg" alt="Dopamine Nation"/>
     </a>
 
 
@@ -124,51 +124,51 @@
 
 
 	<a href="https://cdn.marvel.com/content/1x/gods2023003_cover.jpg" class="thumbnail">
-		<img src="https://cdn.marvel.com/content/1x/gods2023003_cover.jpg" alt="Jonathan Hickman's G.O.D.S">
+		<img src="https://cdn.marvel.com/content/1x/gods2023003_cover.jpg" alt="Jonathan Hickman's G.O.D.S"/>
 	</a>
 
 
     <a href="https://upload.wikimedia.org/wikipedia/en/4/4b/Neuromancer_%28Book%29.jpg" class="thumbnail">
-        <img src="https://upload.wikimedia.org/wikipedia/en/4/4b/Neuromancer_%28Book%29.jpg" alt="Neuromancer">
+        <img src="https://upload.wikimedia.org/wikipedia/en/4/4b/Neuromancer_%28Book%29.jpg" alt="Neuromancer"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/51jgKXaj3tL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/51jgKXaj3tL._AC_UF894,1000_QL80_.jpg" alt="Trauma and Recovery">
+        <img src="https://m.media-amazon.com/images/I/51jgKXaj3tL._AC_UF894,1000_QL80_.jpg" alt="Trauma and Recovery"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/71wxNktCqKL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/71wxNktCqKL._AC_UF894,1000_QL80_.jpg" alt="Moonwalking with Einstein">
+        <img src="https://m.media-amazon.com/images/I/71wxNktCqKL._AC_UF894,1000_QL80_.jpg" alt="Moonwalking with Einstein"/>
     </a>
 
 
     <a href="https://m.media-amazon.com/images/I/5101IFXo3+L._SL500_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/5101IFXo3+L._SL500_.jpg" alt="Old man's War">
+        <img src="https://m.media-amazon.com/images/I/5101IFXo3+L._SL500_.jpg" alt="Old man's War"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/810aUBwcFIL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/810aUBwcFIL._AC_UF1000,1000_QL80_.jpg" alt="Black Cat">
+        <img src="https://m.media-amazon.com/images/I/810aUBwcFIL._AC_UF1000,1000_QL80_.jpg" alt="Black Cat"/>
     </a>
 
 
     <a href="https://cdn.kobo.com/book-images/8a6c41df-530b-4522-8643-928819a16a33/1200/1200/False/mr-and-mrs-x-vol-2.jpg" class="thumbnail">
-        <img src="https://cdn.kobo.com/book-images/8a6c41df-530b-4522-8643-928819a16a33/1200/1200/False/mr-and-mrs-x-vol-2.jpg" alt="Mr and Mrs.X">
+        <img src="https://cdn.kobo.com/book-images/8a6c41df-530b-4522-8643-928819a16a33/1200/1200/False/mr-and-mrs-x-vol-2.jpg" alt="Mr and Mrs.X"/>
     </a>
 
     <a href="https://media.s-bol.com/3vqvo3v118o4/JBL6M2/544x840.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/3vqvo3v118o4/JBL6M2/544x840.jpg" alt="Antifragile">
+        <img src="https://media.s-bol.com/3vqvo3v118o4/JBL6M2/544x840.jpg" alt="Antifragile"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/411MSvCZl7L.jpg" class="thumbnai">
-        <img src="https://m.media-amazon.com/images/I/411MSvCZl7L.jpg" alt="Sanguinius">
+        <img src="https://m.media-amazon.com/images/I/411MSvCZl7L.jpg" alt="Sanguinius"/>
     </a>
 
 
     <a href="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1419196845i/23998360.jpg" class="thumbnail">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1419196845i/23998360.jpg" alt="Open Her">
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1419196845i/23998360.jpg" alt="Open Her"/>
     </a>
 
     <a href="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" class="thumbnail">
-        <img src="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" alt="Basiskennis Wiskunde">
+        <img src="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" alt="Basiskennis Wiskunde"/>
     </a>
 
 
@@ -179,27 +179,27 @@
 
 <div class="july books" align='center'>
     <a href="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1552228430i/40591670.jpg" class="thumbnail">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1552228430i/40591670.jpg" alt="I will Teach you to be rich">
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1552228430i/40591670.jpg" alt="I will Teach you to be rich"/>
     </a>
 
     <a href="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344269423i/13037942.jpg" class="thumbnail">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344269423i/13037942.jpg" alt="The Admirals">
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344269423i/13037942.jpg" alt="The Admirals"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81rsUQ+uy6L._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81rsUQ+uy6L._AC_UF894,1000_QL80_.jpg" alt="Decorum">
+        <img src="https://m.media-amazon.com/images/I/81rsUQ+uy6L._AC_UF894,1000_QL80_.jpg" alt="Decorum"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/516IRl0taeL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/516IRl0taeL._AC_UF1000,1000_QL80_.jpg" alt="Improve your Social Skills">
+        <img src="https://m.media-amazon.com/images/I/516IRl0taeL._AC_UF1000,1000_QL80_.jpg" alt="Improve your Social Skills"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/8135QxIueIL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/8135QxIueIL._AC_UF1000,1000_QL80_.jpg" alt="Small Gods">
+        <img src="https://m.media-amazon.com/images/I/8135QxIueIL._AC_UF1000,1000_QL80_.jpg" alt="Small Gods"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81hqu1dMckL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81hqu1dMckL._AC_UF894,1000_QL80_.jpg" alt="The Light Fantastic">
+        <img src="https://m.media-amazon.com/images/I/81hqu1dMckL._AC_UF894,1000_QL80_.jpg" alt="The Light Fantastic"/>
     </a>
 
 
@@ -209,76 +209,76 @@
 
 <div class="Books to read" align="center">
     <a href="https://media.s-bol.com/BRNNqVmLOxAJ/pQgLWlr/548x840.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/BRNNqVmLOxAJ/pQgLWlr/548x840.jpg" alt="My Grandmother's Hands">
+        <img src="https://media.s-bol.com/BRNNqVmLOxAJ/pQgLWlr/548x840.jpg" alt="My Grandmother's Hands"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/71PlneCID6L._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/71PlneCID6L._AC_UF894,1000_QL80_.jpg" alt="Assassinorum: Kingmaker">
+        <img src="https://m.media-amazon.com/images/I/71PlneCID6L._AC_UF894,1000_QL80_.jpg" alt="Assassinorum: Kingmaker"/>
     </a>
 
 
     <a href="https://media.s-bol.com/YNOrn4jjoWq9/n5ZZxn4/794x1200.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/YNOrn4jjoWq9/n5ZZxn4/794x1200.jpg" alt="Old Man's War">
+        <img src="https://media.s-bol.com/YNOrn4jjoWq9/n5ZZxn4/794x1200.jpg" alt="Old Man's War"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/810aUBwcFIL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/810aUBwcFIL._AC_UF1000,1000_QL80_.jpg" alt="Black Cat">
+        <img src="https://m.media-amazon.com/images/I/810aUBwcFIL._AC_UF1000,1000_QL80_.jpg" alt="Black Cat"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/71F7TG9xvrL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/71F7TG9xvrL._AC_UF1000,1000_QL80_.jpg" alt="House of War">
+        <img src="https://m.media-amazon.com/images/I/71F7TG9xvrL._AC_UF1000,1000_QL80_.jpg" alt="House of War"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81p-kNMCkfL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81p-kNMCkfL._AC_UF1000,1000_QL80_.jpg" alt="Terminal Alliance">
+        <img src="https://m.media-amazon.com/images/I/81p-kNMCkfL._AC_UF1000,1000_QL80_.jpg" alt="Terminal Alliance"/>
     </a>
 
     <a href="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344269423i/13037942.jpg" class="thumbnail">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344269423i/13037942.jpg" alt="The Admirals">
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344269423i/13037942.jpg" alt="The Admirals"/>
     </a>
 
 
     <a href="https://media.s-bol.com/mVO50kEOW4MG/B53rJx/771x1200.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/mVO50kEOW4MG/B53rJx/771x1200.jpg" alt="On War">
+        <img src="https://media.s-bol.com/mVO50kEOW4MG/B53rJx/771x1200.jpg" alt="On War"/>
     </a>
 
     <a href="https://media.s-bol.com/733ONXDrR81y/2vRD14z/693x1200.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/733ONXDrR81y/2vRD14z/693x1200.jpg" alt="The Prince">
+        <img src="https://media.s-bol.com/733ONXDrR81y/2vRD14z/693x1200.jpg" alt="The Prince"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81DFDGzHZqL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81DFDGzHZqL._AC_UF894,1000_QL80_.jpg" alt="Meditations">
+        <img src="https://m.media-amazon.com/images/I/81DFDGzHZqL._AC_UF894,1000_QL80_.jpg" alt="Meditations"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/71G1koXSoiL._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/71G1koXSoiL._AC_UF894,1000_QL80_.jpg" alt="I will teach you to be rich">
+        <img src="https://m.media-amazon.com/images/I/71G1koXSoiL._AC_UF894,1000_QL80_.jpg" alt="I will teach you to be rich"/>
     </a>
 
 
     <a href="https://m.media-amazon.com/images/I/817ws4SipTL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/817ws4SipTL._AC_UF1000,1000_QL80_.jpg" alt="The High Republic">
+        <img src="https://m.media-amazon.com/images/I/817ws4SipTL._AC_UF1000,1000_QL80_.jpg" alt="The High Republic"/>
     </a>
 
     <a href="https://media.s-bol.com/By6GZ6VJwVJX/2jJlyv/546x840.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/By6GZ6VJwVJX/2jJlyv/546x840.jpg" alt="Dreams from my father">
+        <img src="https://media.s-bol.com/By6GZ6VJwVJX/2jJlyv/546x840.jpg" alt="Dreams from my father"/>
     </a>
 
     <a href="https://media.s-bol.com/G9LLYNgM6N68/811x1200.jpg" class="'thumbnail">
-        <img src="https://media.s-bol.com/G9LLYNgM6N68/811x1200.jpg" alt="Warriors of God">
+        <img src="https://media.s-bol.com/G9LLYNgM6N68/811x1200.jpg" alt="Warriors of God"/>
     </a>
 
     
 
     <a href="https://m.media-amazon.com/images/I/71o3pRTuY+L._AC_UF894,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/71o3pRTuY+L._AC_UF894,1000_QL80_.jpg" alt="Assassin's Apprentice">
+        <img src="https://m.media-amazon.com/images/I/71o3pRTuY+L._AC_UF894,1000_QL80_.jpg" alt="Assassin's Apprentice"/>
     </a>
 
     <a href="https://m.media-amazon.com/images/I/81SZzGUi3sL._AC_UF1000,1000_QL80_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/I/81SZzGUi3sL._AC_UF1000,1000_QL80_.jpg" alt="Washington the Indispensable Man">
+        <img src="https://m.media-amazon.com/images/I/81SZzGUi3sL._AC_UF1000,1000_QL80_.jpg" alt="Washington the Indispensable Man"/>
     </a>
 
     <a hreg="https://media.s-bol.com/mngDvk5xDMkr/XZVK5W/550x833.jpg" class="thumbnail">
-        <img src="https://media.s-bol.com/mngDvk5xDMkr/XZVK5W/550x833.jpg" alt="Algorithms to live by">
+        <img src="https://media.s-bol.com/mngDvk5xDMkr/XZVK5W/550x833.jpg" alt="Algorithms to live by"/>
     </a>
 
 
