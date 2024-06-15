5392a063ff03e8efe58b67edf6ca7b6b75ecac6f
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-06-15 11:10:06 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -40,16 +40,24 @@
 
 </div>
 
-<div class="Current game" align="center">
+<div class="June and July games" align="center">
 
 <a href="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" class="thumbnail">
-    <img src="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" alt="Final Fantasys 7 rebirth">
+    <img src="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" alt="Final Fantasy 7 rebirth">
+</a>
+
+<a href="https://jagatplay.com/wp-content/uploads/2024/02/shadow-of-the-erdtree-elden-ring-first-trailer.jpg" class="thumbnail">
+    <img src="https://jagatplay.com/wp-content/uploads/2024/02/shadow-of-the-erdtree-elden-ring-first-trailer.jpg" alt="Shadow of the Erdtree">
+</a>
+
+<a href="https://cdn.webshopapp.com/shops/47899/files/451753660/atlus-ps5-shin-megami-tensei-v-vengeance.jpg" class="thumbnail">
+    <img src="https://cdn.webshopapp.com/shops/47899/files/451753660/atlus-ps5-shin-megami-tensei-v-vengeance.jpg" alt="Shin Megami Tensei Vengeance">
 </a>
 
 </div>
 
 
-<div class="scroll-container2" align="center">
+<div alt="2024 games" align="center">
 
 <a href="https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg" class="thumbnail">
     <img src="https://image.api.playstation.com/vulcan/ap/rnd/202302/2321/3098481c9164bb5f33069b37e49fba1a572ea3b89971ee7b.jpg" alt="Baldur's Gate 3">
@@ -63,6 +71,10 @@
     <img src="https://image.api.playstation.com/vulcan/ap/rnd/202402/0713/5eec986de2675a1514f58d8ba40f3ee13a6faeefc4cf6d30.png" alt="Elden Ring: Shadow of the Erdtree">
 </a>
 
+<a href="https://cdn1.epicgames.com/spt-assets/ea0a24395cd641d7bbde3e5d78ad462c/hades-ii-acu3y.png" class="thumbnail">
+    <img src="https://cdn1.epicgames.com/spt-assets/ea0a24395cd641d7bbde3e5d78ad462c/hades-ii-acu3y.png" alt="Hades Early access">
+</a>
+
 <a href="https://xxboxnews.blob.core.windows.net/prod/sites/2/2024/06/Hero-e17b08808a857e872276.jpg" class="thumbnail">
 	<img src="https://xxboxnews.blob.core.windows.net/prod/sites/2/2024/06/Hero-e17b08808a857e872276.jpg" alt="Life is Strange Double Exposure">
 </a>
@@ -70,7 +82,7 @@
 
 </div>
 
-<div class="scroll-container3" align="center">
+<div alt="2025 and beyond games" align="center">
 
 <a href="https://sm.ign.com/ign_nl/news/p/persona-3-/persona-3-4-and-5-were-blue-yellow-and-red-and-persona-6-wil_ryps.jpg" class="thumbnail">
     <img src="https://sm.ign.com/ign_nl/news/p/persona-3-/persona-3-4-and-5-were-blue-yellow-and-red-and-persona-6-wil_ryps.jpg" alt="Persona 6">
@@ -85,9 +97,6 @@
     <img src="https://i.pinimg.com/originals/e1/bf/64/e1bf640df96495a7adeb6bf2f57c1841.jpg" alt="Horizon III">
 </a>
 
-<a href="https://www.datocms-assets.com/56706/1707465398-grand-theft-auto-6.png?auto=format&fit=max&w=1200" class="thumbnail">
-    <img src="https://www.datocms-assets.com/56706/1707465398-grand-theft-auto-6.png?auto=format&fit=max&w=1200" alt="Grand Theft Auto 6">
-</a>
 
 <a href="https://yt3.googleusercontent.com/skAODIZ07KvllI7tXL6ERzwZn1ECRCdIPehY4dV5AvaD9xxu6L2Q-rm6his2ImwQgSGISmJtgg=s900-c-k-c0x00ffffff-no-rj" class="thumbnail">
     <img src="https://yt3.googleusercontent.com/skAODIZ07KvllI7tXL6ERzwZn1ECRCdIPehY4dV5AvaD9xxu6L2Q-rm6his2ImwQgSGISmJtgg=s900-c-k-c0x00ffffff-no-rj" alt="Cyberpunk">
