f6dce0b3f675a124abc3f10801f3d1b330a66863
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-07-20 17:29:39 +0200

    Add files via upload

---
+++ xmen.php
@@ -0,0 +1,98 @@
+<DOCTYPE HTML>
+    <head>
+        <style>
+
+            body{
+                background-image: url("https://cdn.mos.cms.futurecdn.net/ZmMkFCTgBNMoxEohbzBXJm.jpg");
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: 100%;
+            }
+
+        
+        </style>
+    <title>Favourite X-men</title>
+    <link rel="stylesheet" href="styles.css">
+
+    </head>
+
+    <body>
+
+        <div alt="Absolute favourite" align="center">
+            <a href="https://i.pinimg.com/736x/5b/3b/73/5b3b73959d509c1efe1618a9ff92f202.jpg" class="thumbnail">
+                <img src="https://i.pinimg.com/736x/5b/3b/73/5b3b73959d509c1efe1618a9ff92f202.jpg" alt="Storm">
+        </div>
+
+        <div alt="Rest of Mount Rushmore" align="center">
+            <a href="https://i.redd.it/8t7y09h7mzc71.jpg" class="thumbnail">
+                <img src="https://i.redd.it/8t7y09h7mzc71.jpg" alt="Magneto">
+            
+            <a href="https://preview.redd.it/8twwf413aehb1.jpg?width=640&crop=smart&auto=webp&s=76cc547b44ae27adb1558f7b42d8e56e2f67d7b7" class="thumbnail">
+                <img src="https://preview.redd.it/8twwf413aehb1.jpg?width=640&crop=smart&auto=webp&s=76cc547b44ae27adb1558f7b42d8e56e2f67d7b7" alt="Cyclops">
+
+            <a href="https://cafans.b-cdn.net/images/Category_4247/subcat_176995/CS9GID2O_2808182000251gpadd.JPG" class="thumbnail">
+                <img src="https://cafans.b-cdn.net/images/Category_4247/subcat_176995/CS9GID2O_2808182000251gpadd.JPG" alt="Emma Frost">       
+        </div>
+
+        <div alt="Rest of the top 10" align="center">
+            <a href="https://i.pinimg.com/736x/9c/66/f1/9c66f1b956eaf93f4f8d21e34d2a4f5b.jpg" class="thumbnail">
+                <img src="https://i.pinimg.com/736x/9c/66/f1/9c66f1b956eaf93f4f8d21e34d2a4f5b.jpg" alt="Magik">
+            </a>
+
+            <a href="https://i.pinimg.com/736x/77/e8/4b/77e84b318f791093c89822483dae1190.jpg" class="thumbnail">
+                <img src="https://i.pinimg.com/736x/77/e8/4b/77e84b318f791093c89822483dae1190.jpg" alt="Laura Kinney">
+            </a>
+
+            <a href="https://i.pinimg.com/736x/21/40/0e/21400e7825489fc28b6d3f3d05e01a29.jpg" class="thumbnail">
+                <img src="https://i.pinimg.com/736x/21/40/0e/21400e7825489fc28b6d3f3d05e01a29.jpg" alt="Monet now in the top 10">
+            </a>
+
+            <a href="https://the616comics.com/cdn/shop/products/image_1cae950e-08f5-4a67-8f54-d046a19a9589_1200x1200.jpg?v=1677679312" class="thumbnail">
+                <img src="https://the616comics.com/cdn/shop/products/image_1cae950e-08f5-4a67-8f54-d046a19a9589_1200x1200.jpg?v=1677679312" alt="Juggernaut now in the top 10">
+            </a>
+
+            /*<a href="https://64.media.tumblr.com/ef8eeeace24e93c38666f5fa38723e5e/6aee5a075e7f430b-72/s540x810/0790e64eafe3d1a89cafb34d9283e7e75fd87356.png" class="thumbnail">
+                <img src="https://64.media.tumblr.com/ef8eeeace24e93c38666f5fa38723e5e/6aee5a075e7f430b-72/s540x810/0790e64eafe3d1a89cafb34d9283e7e75fd87356.png" alt="Rogue now in the top 10">
+            </a>*/
+
+            <a href="https://gentlemanvillian.wordpress.com/wp-content/uploads/2013/03/tumblr_lqm6fg8pel1qihd3b.jpg" class="thumbnail">
+                <img src="https://gentlemanvillian.wordpress.com/wp-content/uploads/2013/03/tumblr_lqm6fg8pel1qihd3b.jpg" alt="Sage now in the top 10">
+            </a>
+
+        <div alt="Out of the top 10" align="center">
+            <a href="https://i.redd.it/rcbkhbhfd9s91.jpg" class="thumbnail">
+                <img src="https://i.redd.it/rcbkhbhfd9s91.jpg" alt="Apocalypse out of the top 10">
+            </a>
+
+            <a href="https://sportshub.cbsistatic.com/i/2024/05/09/d315504e-9122-4a96-9497-a2a4209e6b63/wolverine-x-men-from-the-ashes-header.jpg" class="thumbnail">
+                <img src="https://sportshub.cbsistatic.com/i/2024/05/09/d315504e-9122-4a96-9497-a2a4209e6b63/wolverine-x-men-from-the-ashes-header.jpg" alt="Logan out of the top 10">
+            </a>
+
+            <a href="https://qph.cf2.quoracdn.net/main-qimg-331b9350fcc442a0ec28ec6ef1b6f7f0" class="thumbnail">
+                <img src="https://qph.cf2.quoracdn.net/main-qimg-331b9350fcc442a0ec28ec6ef1b6f7f0" alt="Nightcrawler">
+            </a>
+
+            <a href="https://uncannyxmen.net/sites/default/files/images/costume/shadowcat-bigcostume4.jpg" class="thumbnail">
+                <img src="https://uncannyxmen.net/sites/default/files/images/costume/shadowcat-bigcostume4.jpg" alt="Shadowcat out of the top 10">
+            </a>
+            
+
+
+
+        <div alt="The canon Mount Rushmore" align="center">
+            <a href="https://preview.redd.it/jellc6aa1rlb1.jpg?auto=webp&s=21be4a3c6b475902a08dee167930e6f1ccc1c557" class="thumbnail">
+                <img src="https://preview.redd.it/jellc6aa1rlb1.jpg?auto=webp&s=21be4a3c6b475902a08dee167930e6f1ccc1c557" alt="Xavier">
+            
+            <a href="https://preview.redd.it/holy-shit-magneto-is-a-total-hunk-right-now-so-he-is-v0-mvu3i94ck8uc1.png?width=464&format=png&auto=webp&s=73af7d9de83a80ad791300cb467359c34e818c35" class="thumbnail">
+                <img src="https://preview.redd.it/holy-shit-magneto-is-a-total-hunk-right-now-so-he-is-v0-mvu3i94ck8uc1.png?width=464&format=png&auto=webp&s=73af7d9de83a80ad791300cb467359c34e818c35" alt="Magneto">
+
+            <a href="https://blacknerdproblems.com/wp-content/uploads/2014/09/Cable_and_X-Force_Vol_1_7_Textless-1.jpg" class="thumbnail">
+                <img src="https://blacknerdproblems.com/wp-content/uploads/2014/09/Cable_and_X-Force_Vol_1_7_Textless-1.jpg" alt="Cyclops"> 
+
+            <a href="https://pbs.twimg.com/media/FOuQw0VXEBk1j3t.jpg" class="thumbnail">
+                <img src="https://pbs.twimg.com/media/FOuQw0VXEBk1j3t.jpg" alt="Storm">
+        </div>
+
+    </body>
+
+</html>
