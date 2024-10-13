e061c3ef8201d928c713dd09998628cef3212be6
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-10-14 00:57:33 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -44,6 +44,17 @@
 
 <div alt="current game" align="center">
 
+<a href="https://metaphor.atlus.com/media/img/hero/hero-art.jpg" class="thumbnail">
+    <img src="https://metaphor.atlus.com/media/img/hero/hero-art.jpg" alt="Metaphor Re:Fantazio, October">
+</a>
+
+
+
+ 
+</div>
+
+<div alt="Other games" align="center">
+
 <a href="https://static1.srcdn.com/wordpress/wp-content/uploads/2023/08/how-to-romance-shadowheart-in-baldur-s-gate-3.jpg" class="thumbnail">
     <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2023/08/how-to-romance-shadowheart-in-baldur-s-gate-3.jpg" alt="Baldur's Gate 3">
 </a>
@@ -53,31 +64,10 @@
 </a>
 
 
- 
-</div>
-
-<div alt="Other games" align="center">
-
-<a href="https://metaphor.atlus.com/media/img/hero/hero-art.jpg" class="thumbnail">
-    <img src="https://metaphor.atlus.com/media/img/hero/hero-art.jpg" alt="Metaphor Re:Fantazio">
-</a>
-
-<a href="https://gh.cdn.sewest.net/assets/ident/news/6abbb7d1/01-Max-Caulfield-Hero-Shot-2mkwl79i0.jpg?quality=65" class="thumbnail">
-    <img src="https://gh.cdn.sewest.net/assets/ident/news/6abbb7d1/01-Max-Caulfield-Hero-Shot-2mkwl79i0.jpg?quality=65" alt="Life is Strange: Double Exposure">
-</a>
-
-<a href="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/09/How-Long-Does-It-Takes-To-Beat-Chrono-Trigger.jpg" class="thumbnail">
-    <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2021/09/How-Long-Does-It-Takes-To-Beat-Chrono-Trigger.jpg" alt="Chrono Trigger">
-</a>
-
-<a href="https://sm.ign.com/ign_nl/photo/h/hades-2-te/hades-2-technical-test-announced-heres-how-to-join_7x4j.jpg" class="thumbnail">
-    <img src="https://sm.ign.com/ign_nl/photo/h/hades-2-te/hades-2-technical-test-announced-heres-how-to-join_7x4j.jpg" alt="Hades II">
-</a>
-
-
 
 
 </div>
+
 
 
 
@@ -105,32 +95,6 @@
 
 
 
-<div alt="Unknown and beyond games" align="center">
-
-<a href="https://assets.xboxservices.com/assets/38/74/38740001-90b4-4404-83bb-154475213631.jpg?n=74189653_GLP-Page-Hero-1084_1920x1080.jpg" class="thumbnail">
-    <img src="https://assets.xboxservices.com/assets/38/74/38740001-90b4-4404-83bb-154475213631.jpg?n=74189653_GLP-Page-Hero-1084_1920x1080.jpg" alt="Expedition 33">
-</a>
-
-
-<a href="https://sportshub.cbsistatic.com/i/2024/07/25/9eaf99e8-fbd4-4e41-880b-8c337db7be80/stellar-blade.jpg" class="thumbnail">
-    <img src="https://sportshub.cbsistatic.com/i/2024/07/25/9eaf99e8-fbd4-4e41-880b-8c337db7be80/stellar-blade.jpg" alt="Stellar Blade sequel">
-</a>
-
-<a href="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/08/persona-protage.jpg" class="thumbnail">
-    <img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/08/persona-protage.jpg" alt="Persona 6">
-</a>
-
-<a href="https://assetsio.gnwcdn.com/cyberpunk-2077-new-v-crop.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp" class="thumbnail">
-    <img src="https://assetsio.gnwcdn.com/cyberpunk-2077-new-v-crop.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp" alt="Cyberpunk sequel">
-</a>
-
-<a href="https://i.pinimg.com/originals/e1/bf/64/e1bf640df96495a7adeb6bf2f57c1841.jpg" class="thumbnail">
-    <img src="https://i.pinimg.com/originals/e1/bf/64/e1bf640df96495a7adeb6bf2f57c1841.jpg" alt="Horizon III. ">
-</a>
-
-
-
-</div>
 
 
 <div alt="Competitive games" align="center">
