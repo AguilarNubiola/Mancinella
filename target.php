a9f0411d7b2fdd601d50685844b77fe74ed6b447
AguilarNubiola <145224814+AguilarNubiola@users.noreply.github.com>
2024-12-01 01:29:18 +0100

    Add files via upload

--- target.php
+++ target.php
@@ -36,19 +36,13 @@
         <img src="https://cdn.bap-software.net/2023/10/softwaretesting1.jpg" alt="Software testing">
     </a>
 
-    <a href="https://www.marketingfacts.nl/wp-content/uploads/2023/09/Odido-Logo-aspect-ratio-943-471.5-1.png" class="thumbnail">
-        <img src="https://www.marketingfacts.nl/wp-content/uploads/2023/09/Odido-Logo-aspect-ratio-943-471.5-1.png" alt="Odido">
+    <a href="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" class="thumbnail">
+        <img src="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" alt="Basiskennis Wiskunde">
     </a>
 
-    <a href="https://images-3.rakuten.tv/storage/snapshot/shot/a2431a65-c873-4b6b-a83d-dd2cb50f1b3c-snapshot-1590665896.jpeg" class="thumbnail">
-        <img src="https://images-3.rakuten.tv/storage/snapshot/shot/a2431a65-c873-4b6b-a83d-dd2cb50f1b3c-snapshot-1590665896.jpeg" alt="Lust Caution">
+    <a href="https://nuevalengua.com/wp-content/uploads/2021/12/Que-chevere-el-espanol-colombiano-Nueva-Lengua.jpg" class="thumbnail">
+        <img src="https://nuevalengua.com/wp-content/uploads/2021/12/Que-chevere-el-espanol-colombiano-Nueva-Lengua.jpg" alt="Espanol Colombiano">
     </a>
-
-    
-
-    
-    
-
 
 
 
@@ -56,87 +50,21 @@
 
 <div alt="Next up" align="center">
 
-    <a href="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" class="thumbnail">
-        <img src="https://syntaxmedia.nl/wp-content/uploads/2023/03/wiskunde-voor-bachelor-en-master-1-basiskennis-en-basisvaardigheden-jpg.webp" alt="Basiskennis Wiskunde">
-    </a>
-
-    <a href="https://media.s-bol.com/gKjkMlJjgVG3/WG1lvv/964x1200.jpg" class="thumbnail">
+<a href="https://media.s-bol.com/gKjkMlJjgVG3/WG1lvv/964x1200.jpg" class="thumbnail">
         <img src="https://media.s-bol.com/gKjkMlJjgVG3/WG1lvv/964x1200.jpg" alt="PHP & MYSQL">
     </a>
 
+
     <a href="https://media.timeout.com/images/105804574/image.jpg" class="thumbnail">
-        <img src="https://media.timeout.com/images/105804574/image.jpg" alt="Rebuild of Evangelion">
-    </a>
+    <img src="https://media.timeout.com/images/105804574/image.jpg" alt="Rebuild of Evangelion">
+</a>
 
-    <a href="https://pbs.twimg.com/media/EcLdRTvX0AEMwt3?format=jpg&name=4096x4096" class="thumbnail">
+<a href="https://pbs.twimg.com/media/EcLdRTvX0AEMwt3?format=jpg&name=4096x4096" class="thumbnail">
         <img src="https://pbs.twimg.com/media/EcLdRTvX0AEMwt3?format=jpg&name=4096x4096" alt="Yamamoto">
-     </a>
-
-    <a href="https://ew.com/thmb/uhFZz2n0Wgr9Zq_zebe1SypeZA8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-handmaiden-review-ew-906481c7a21f4aa8b6f19849f86c97eb.jpg" class="thumbnail">
-        <img src="https://ew.com/thmb/uhFZz2n0Wgr9Zq_zebe1SypeZA8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-handmaiden-review-ew-906481c7a21f4aa8b6f19849f86c97eb.jpg" alt="The Handmaiden">
-    </a>
-
-    <a href="https://images-eu.ssl-images-amazon.com/images/I/91WLf3QOBiL._AC_UL600_SR600,600_.jpg" class="thumbnail">
-        <img src="https://images-eu.ssl-images-amazon.com/images/I/91WLf3QOBiL._AC_UL600_SR600,600_.jpg" alt="X-Factor Omnibus">
-    </a>
-
-    <a href="https://assets.lastdodo.com/image/ld_large/plain/assets/catalog/assets/2018/6/22/5/5/d/pdf_55d76a06-760c-11e8-9273-d6543d870e1d.jpg" class="thumbnail">
-        <img src="https://assets.lastdodo.com/image/ld_large/plain/assets/catalog/assets/2018/6/22/5/5/d/pdf_55d76a06-760c-11e8-9273-d6543d870e1d.jpg" alt="Fables deel 10">
     </a>
 </div>
 
 
-
-
-<div alt="November targets" align="center">
-
-
-    <a href="https://i.imgur.com/xjRSngh.jpeg" class="thumbnail">
-        <img src="https://i.imgur.com/xjRSngh.jpeg" alt="Queen of Knives">
-    </a>
-
-    <a href="https://cdn.mos.cms.futurecdn.net/jUzkZX6peuBa5BoMKofEDT.jpg" class="thumbnail">
-        <img src="https://cdn.mos.cms.futurecdn.net/jUzkZX6peuBa5BoMKofEDT.jpg" alt="Baldur's Gate 3">
-    </a>
-
-    
-< div alt="trophies" class="thumbnail">
-    
-    <a href="https://m.media-amazon.com/images/M/MV5BMjg3N2M3OWUtZGQ3NC00OGI1LTllZDUtMTdiYWQxMTk0YTg2XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/M/MV5BMjg3N2M3OWUtZGQ3NC00OGI1LTllZDUtMTdiYWQxMTk0YTg2XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg" alt="No hard feelings">
-    </a>
-
-    <a href="https://m.media-amazon.com/images/M/MV5BODQxNTUyZWYtN2QyNS00NTMyLTlmNmQtYTg1M2FjOThkZTc5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" class="thumbnail">
-        <img src="https://m.media-amazon.com/images/M/MV5BODQxNTUyZWYtN2QyNS00NTMyLTlmNmQtYTg1M2FjOThkZTc5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" alt="If you were the last">
-    </a>
-
-    <a href="https://i.guim.co.uk/img/media/18e82d07c98a71876f7fb81618e043f78b96626b/0_0_1822_1093/master/1822.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=6396af52cb2424d96ae829ed159929f7" class="thumbnail">
-       <img src="https://i.guim.co.uk/img/media/18e82d07c98a71876f7fb81618e043f78b96626b/0_0_1822_1093/master/1822.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=6396af52cb2424d96ae829ed159929f7" alt="Bound">
-    </a>
-
-    <a href="https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TalesOfKenzeraZau.jpg" class="thumbnail">
-       <img src="https://www.nintendo.com/eu/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TalesOfKenzeraZau.jpg" alt="Tales of Kenzera:Zau">
-    </a>
-
-    <a href="https://i0.wp.com/insider-gaming.com/wp-content/uploads/2024/02/stellar-blade-eve.jpg?fit=2560%2C1440&ssl=1" class="thumbnail">
-        <img src="https://i0.wp.com/insider-gaming.com/wp-content/uploads/2024/02/stellar-blade-eve.jpg?fit=2560%2C1440&ssl=1" alt="Stellar Blade">
-    </a>
-
-    <a href="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" class="thumbnail">
-    <img src="https://www.notebookcheck.nl/fileadmin/Notebooks/News/_nc4/Final-Fantasy-7-Rebirth.jpg" alt="Final Fantasy 7 rebirth">
-    </a>
-
-    <a href="https://image.api.playstation.com/vulcan/ap/rnd/202402/0713/5eec986de2675a1514f58d8ba40f3ee13a6faeefc4cf6d30.png" class="thumbnail">
-    <img src="https://image.api.playstation.com/vulcan/ap/rnd/202402/0713/5eec986de2675a1514f58d8ba40f3ee13a6faeefc4cf6d30.png" alt="Erdtree">
-    </a>
-
-    <a href="https://metaphor.atlus.com/media/img/hero/hero-art.jpg" class="thumbnail">
-    <img src="https://metaphor.atlus.com/media/img/hero/hero-art.jpg" alt="Metaphor Re:Fantazio">
-    </a>
-    
-</div>
-
-</div>
 
 
 
